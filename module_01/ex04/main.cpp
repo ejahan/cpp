e9cef3c4f72190256863b0d69d3156c21dc7e09a
Elisa JAHAN <ejahan@e2r2p3.clusters.42paris.fr>
2022-05-04 01:41:53 +0200

    fin ex04 module01

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -6,31 +6,64 @@
 /*   By: ejahan <ejahan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 17:01:22 by ejahan            #+#    #+#             */
-/*   Updated: 2022/05/03 06:41:17 by ejahan           ###   ########.fr       */
+/*   Updated: 2022/05/04 01:38:09 by ejahan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "replace_file.hpp"
 #include <fstream>
+#include <cstring>
+
+void	check_to_replace(std::string line, std::string *file, std::string s1, std::string s2)
+{
+	int	i;
+
+	i = 0;
+	while (i < line.length())
+	{
+		if (line.compare(i, s1.length(), s1) == 0)
+		{
+			i += s1.length();
+			*file += s2;
+		}
+		else
+		{
+			*file += line[i];
+			i++;
+		}
+	}
+	*file += '\n';
+	return ;
+}
 
 int	main(int ac, char **av)
 {
-	std::string	file;
-
 	if (ac != 4)
 	{
 		std::cout << "wrong number of arguments" << std::endl;
-		return (-1);
+		return (1);
 	}
-	std::string _filename = av[1];
-	std::string _s1 = av[2];
-	std::string _s2 = av[3];
-	std::ifstream	ifs(_filename);
-	ifs >> file;
+	std::string		filename = av[1];
+	std::string		s1 = av[2];
+	std::string		s2 = av[3];
+	std::string		line;
+	std::string		new_file_content;
+	if (s1.empty() || s2.empty())
+	{
+		std::cout << "ERROR : s1 and s2 should not be empty" << std::endl;
+		return (1);
+	}
+	std::ifstream	ifs(filename);
+	if (!ifs)
+	{
+		std::cout << "ERROR : file doesn't exist" << std::endl;
+		return (1);
+	}
+	std::ofstream	ofs(filename += ".replace");
+	while (getline(ifs, line))
+		check_to_replace(line, &new_file_content, s1, s2);
+	ofs << new_file_content;
 	ifs.close();
-	_filename += ".replace";
-	std::ofstream	ofs(_filename);
-	ofs << file << std::endl;
-	ofs.close();	
+	ofs.close();
 	return (0);
 }
